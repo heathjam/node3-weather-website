b0d1cc3163bed6fc81ea5bd1e101e704c76641cc
Paul Heath <paulheath10@hotmail.com>
2020-09-16 10:29:23 -0500

    Updated weather forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,20 +1,5 @@
 const request = require('request')
 
-// const url = 'http://api.weatherstack.com/current?access_key=91a85ed249d424c090f0ffcfe2ce9bd1&query=37.8267,-122.4233'
-
-// request({ url: url, json: true }, (error, response) => {
-//     if(error){
-//         console.log('unable to connect to weather service')
-//     }else if (response.body.error){
-//         console.log('Unable to find location')
-//     }else{
-//         const temp = response.body.current.temperature
-//         const precip = response.body.current.precip
-//         console.log('It is currently ' + temp + ' degrees out. There is a ' + precip + '% chance of rain.')
-//     }
-// })
-
-
 const forecast = (lat, long, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=91a85ed249d424c090f0ffcfe2ce9bd1&query=' + lat + ',' + long
 
@@ -24,7 +9,7 @@ const forecast = (lat, long, callback) => {
         }else if(body.error){
             callback('Unable to find location')
         }else{
-            callback(undefined, 'It is currently ' + body.current.temperature + '. There is a ' + body.current.precip + '% chance of rain.')
+            callback(undefined, 'It is currently ' + body.current.temperature + 'C and feels like ' +  body.current.feelslike  + 'C. The sky is ' + body.current.weather_descriptions[0] +   ' and there is a ' + body.current.precip + '% chance of rain.')
         }
     })
 }
